89355dec738c94937db71fdde62f29bdc44e4778
maharaaniii <umimaharani08@gmail.com>
2025-04-28 20:44:13 +0700

    Jobsheet Praktikum ALSD

---
+++ Mahasiswa23.java
@@ -0,0 +1,21 @@
+public class Mahasiswa23 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa23 () {
+
+    }
+
+    public Mahasiswa23(String nama, String nim, String kelas) {
+       this.nama = nama;
+       this.nim = nim;
+       this.kelas = kelas;
+       nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
